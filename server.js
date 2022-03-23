26fbb0effe8971caf4417db245c7d45f417d653d
Heron Amaral <heron.amaral@gmail.com>
2022-03-22 21:51:15 -0300

    env variables settings

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-server.listen(3000);
+server.listen(process.env.PORT);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
